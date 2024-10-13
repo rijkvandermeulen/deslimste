3c5e8107d296a0448b60d3b9b57c023e81eba2fb
Rijk van der Meulen <rijk.vandermeulen@eyeon.nl>
2024-10-13 10:37:57 +0200

    navigate between questions

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    path('quiz/<int:game_id>/', views.quiz_view, name='quiz'),
+    path('quiz/<int:game_id>/<int:current_question_id>/', views.quiz_view, name='quiz'),
     path('admin/', admin.site.urls),
 ]
--- app/views.py
+++ app/views.py
@@ -3,11 +3,12 @@ from django.shortcuts import render, get_object_or_404
 from app.models import Game
 
 
-def quiz_view(request, game_id):
+def quiz_view(request, game_id, current_question_id=0):
     game = get_object_or_404(Game, id=game_id)
-    current_question = game.question_set.questions.first()
+    current_question = game.question_set.questions.all()[current_question_id]
     context = {
         'game': game,
         'current_question': current_question,
+        'current_question_id': current_question_id,
     }
     return render(request, 'app/quiz.html', context)
